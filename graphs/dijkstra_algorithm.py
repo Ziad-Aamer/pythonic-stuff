b6f42ed5c794a4e28aec7702281678cb5d8e0003
Ziad Aamer <Ziad.Aamer@viavisolutions.com>
2023-03-15 13:55:47 +0200

    Add Dijkstra algorithm v2

--- graphs/dijkstra_algorithm.py
+++ graphs/dijkstra_algorithm.py
@@ -15,7 +15,7 @@ Notes:
 """
 
 
-def run_dijkstra(graph, start_node, end_node):
+def run_dijkstra_v1(graph, start_node, end_node):
     nodes_list = {}
     for key in graph.keys():
         nodes_list[key] = {'cost': 99999, 'parent': '', 'visited': False}
@@ -24,10 +24,13 @@ def run_dijkstra(graph, start_node, end_node):
 
     #TODO: make sure we check all nodes, and that this condition is correct
     while ch_node != -1:
+
         # Update neighbours
         for node in graph[ch_node]:
-            nodes_list[node]['cost'] = graph[ch_node][node]
-            nodes_list[node]['parent'] = ch_node
+            cost = nodes_list[ch_node]['cost'] + graph[ch_node][node]
+            if cost < nodes_list[node]['cost']:
+                nodes_list[node]['cost'] = cost
+                nodes_list[node]['parent'] = ch_node
         nodes_list[ch_node]['visited'] = True
 
         # Find cheapest node.
@@ -39,6 +42,7 @@ def run_dijkstra(graph, start_node, end_node):
                 ch_node = key
                 cheapest = node['cost']
 
+    print("Min cost: ", nodes_list[end_node]['cost'])
     # Get the path
     node = end_node
     path = [end_node]
@@ -48,6 +52,60 @@ def run_dijkstra(graph, start_node, end_node):
 
     path.reverse()
     return path
+
+
+def get_path(start, end, parent):
+    # Get the path
+    node = end
+    path = [end]
+    while node != start:
+        node = parent[node]
+        path.append(node)
+
+    path.reverse()
+    return path
+
+
+def find_lowest_cost_node(costs, processed):
+    lowest_cost = float("inf")
+    lowest_node = None
+    for node in costs:
+        cost = costs[node]
+        if lowest_cost > cost and node not in processed:
+            lowest_cost = cost
+            lowest_node = node
+
+    return lowest_node
+
+
+def run_dijkstra_v2(graph, start, end):
+
+    # 1- initialize
+    costs = {}
+    parents = {}
+    processed = []
+
+    for node in graph.keys():
+        if node in graph[start]:
+            costs[node] = graph[start][node]
+            parents[node] = start
+        else:
+            costs[node] = float("inf")
+            parents[node] = None
+
+    # 2- start algo
+    node = find_lowest_cost_node(costs, processed)
+    while node is not None:
+        for n_node in graph[node]:
+            cost = costs[node] + graph[node][n_node]
+            if cost < costs[n_node]:
+                costs[n_node] = cost
+                parents[n_node] = node
+        processed.append(node)
+        node = find_lowest_cost_node(costs, processed)
+
+    print("Min cost: ", costs[end])
+    return get_path(start, end, parents)
 
 def test_dijkstra():
     graph = {}
@@ -61,6 +119,17 @@ def test_dijkstra():
     graph["b"]["fin"] = 5
     graph["fin"] = {}
 
-    path = run_dijkstra(graph, "start", "fin")
-
+    path = run_dijkstra_v1(graph, "start", "fin")
     print("Path: ", path)
+
+    path = run_dijkstra_v2(graph, "start", "fin")
+    print("Path: ", path)
+
+"""
+Dividing the algorithm into functions will:
+    1- Make it more readable
+    2- avoid mistakes
+    3- give you overview
+    4- abstract, so it is easier to remember
+"""
+
