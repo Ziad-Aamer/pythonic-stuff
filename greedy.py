ec2777caca05d2f9d5dd70564ba79770d03926ab
ziad <ziadaamer9@gmail.com>
2023-03-17 11:31:24 +0200

    Add notes

---
+++ greedy.py
@@ -0,0 +1,7 @@
+"""
+Greedy algorithms: 
+    *Idea: at each step you pick the locally optimal solution, 
+    and you end up with the globally optiomal solution.
+    
+    *Greedy aren't perfect, but they are pretty close.
+"""
