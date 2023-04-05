84c312cd263dd9475052330586f4be2da44c8ccb
Ziad Aamer <Ziad.Aamer@viavisolutions.com>
2023-04-05 17:50:28 +0200

    add dynamic programming starter

---
+++ dynamic-programming/dynamic_programming.py
@@ -0,0 +1,10 @@
+"""
+Dynamic programming:
+    *solving subproblems and builds up to solving the big problem.
+
+    *Handling items that depend on each other:
+        Dynamic programming is powerful because it can solve
+        subproblems and use those answers to solve the big problem. Dynamic
+        programming only works when each subproblem is discrete—when it
+        doesn’t depend on other subproblems.
+"""
--- greedy.py
+++ greedy.py
@@ -1,7 +1,7 @@
 """
 Greedy algorithms: 
     * Idea: at each step you pick the locally optimal solution,
-    and you end up with the globally optiomal solution.
+    and you hope to end up with a globally optiomal solution.
     
     * Greedy aren't perfect, but they are pretty close.
 
