0dbe4a9a6daec9ee3352aa4aa51e4c74d50f8e3e
ziad <ziadaamer9@gmail.com>
2023-05-12 12:41:14 +0300

    feat: add KNN and general-algo

---
+++ general-algos.py
@@ -0,0 +1,16 @@
+"""
+*Binary search tree:
+    -insert an item to its right position in the tree to stay sorted.
+    -for every node, the nodes on the left are smaller, and the nodes on the right are larger.
+*Balanced binary search trees: red-black trees, AVL-trees.
+*Trees: B-trees, Heaps, Splay trees
+
+*Inverted index: a hash that maps words to places where they appear. (used in search engines)
+
+*Fourier transform: given a smoothie, the Fourier transform will tell you the ingredients in the smoothie.
+
+*Parallel Programming: 
+    -Overhead of managing the parallelism (dividing, sending, and merging the data) 
+    -Load balancing: each core get an equal work to evenly distribute the work
+
+"""
---
+++ k-nearest-neighbors.py
@@ -0,0 +1,18 @@
+"""
+KNN is used for classification and regression, and involves looking at the k-nearest neighbours.
+
+    -Classification = categorization into groups.
+    -Regresion = predicting (like predicting a fruit is Orange or Grapefruit 
+        based on its features matching with previous samples)
+
+*We can use the ecludian distance to discover how much 2 items are close.
+        
+*Feature extraction means converting an item (like a fruit or user) into a list of numbers (size or color of a fruit) 
+    that can be compared.
+*Not any features will work, we have to pick good features for a successful KNN.
+
+*OCR (Optical character recognition) a photo to text
+
+*Spam filters use another simple algorithm called the Naive Bayes classifier.
+
+"""
