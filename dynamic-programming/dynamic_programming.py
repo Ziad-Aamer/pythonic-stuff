75f409149e954ed842b3aa81a3e2d910439651ec
Ziad Aamer <Ziad.Aamer@viavisolutions.com>
2023-05-21 12:29:53 +0300

    add final chapter

--- dynamic-programming/dynamic_programming.py
+++ dynamic-programming/dynamic_programming.py
@@ -7,4 +7,7 @@ Dynamic programming:
         subproblems and use those answers to solve the big problem. Dynamic
         programming only works when each subproblem is discrete—when it
         doesn’t depend on other subproblems.
+
+    *DP is useful when you are trying to optimize something given a constraint.
+        E.g.: In the knapsack problem, you are maximizing what you can steal given a constrained size of knapsack.
 """
--- general-algos.py
+++ general-algos.py
@@ -13,4 +13,50 @@
     -Overhead of managing the parallelism (dividing, sending, and merging the data) 
     -Load balancing: each core get an equal work to evenly distribute the work
 
+*MapReduce:
+    -Distributed algorithm that can be used through Apache Hadoop.
+    -Why we need it? To run algorithms on distributed machines, to speed up the time required to do it.
+    -Map: takes an array and applies the same function to each item in the array e.g. doubling every item
+        arr1 = [1, 2, 3, 4, 5]
+        >>> arr2 = map(lambda x: 2 * x, arr1)
+        [2, 4, 6, 8, 10]
+
+    -Reduce: reduce a whole list of items down to one item e.g. summing array items e.g.
+        >>> arr1 = [1, 2, 3, 4, 5]
+        >>> reduce(lambda x,y: x+y, arr1)
+        15
+
+    -When you have a large dataset (billions
+    of rows), MapReduce can give you an answer in minutes where a
+    traditional database might take hours
+
+*Bloom filters and HyperLogLog
+    -If you have a lot of data and satisfied with approximate answers, check out probabilistic algorithms!
+    -Bloom filters: probabilistic data structures that
+        -give an answer that could be wrong but is probably correct
+        -take up very little space, but doesn't give exact answer.
+    -HyperLogLog: approximates the number of unique elements in a set
+        -Only take a fraction of the memory
+        -It won't give you the exact answer, but it comes very close.
+
+SHA:
+    -Secure hash algorithm that has many uses cases e.g. comparing 2 files, checking if passwords match, ...
+    -It is a one-way (we can't retrive the original string from the hash) that's why we store the hash of the passwords.
+    -SHA is a family of algorithms, most secure SHA-3. Standard is bcrypt
+    -It's localy insensitive (avalanche effect: changing one character of the string will generate a completely new hash)
+
+Simhash:
+    -it is localy-sensitive, that's why it is used to detect duplicates or check for similar items.
+
+Diffie-Hellman:
+    -Algorithm used to ecrypt/decrypt messages exchanged between 2.
+    -It has 2 keys [public and private]:
+        *The public is used to encrypt, and the encrypted message can only be decrypted using the private key
+    -RSA is its successor.
+
+Liner programming:
+    -is used to maximize something given some constraints.
+    -Can solve all the graph algorithms !!
+    -uses the Simplex algorithm
+
 """
